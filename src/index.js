9bd8d6cf30c68925f459d2f983bd88133f281dba
amir yosef <amiryosef99@outlook.com>
2019-06-15 03:13:35 +0200

    err

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div> Error: {this.State.errorMessage}</div>;
         }
@@ -21,7 +21,16 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat} />
 
         }
-        return <Spinner/>
+        return <Spinner massage="Please accept location request"/>
+    }
+
+    render() {
+        return(
+            <div className="border red">
+            {this.renderContent()}
+            </div>
+        )
+
     }
 }
 
